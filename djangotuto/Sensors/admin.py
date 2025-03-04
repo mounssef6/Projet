1180d6022dd15ef6dc2596d07a00838277c13bdc
mounssef6 <mounssef.rouidi23@ump.ac.ma>
2025-03-05 00:29:41 +0100

    Delete djangotuto directory

--- djangotuto/Sensors/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import SensorData, Device, User # Import your model
-
-# Register your models here.
-admin.site.register(SensorData)
-admin.site.register(Device)
-admin.site.register(User)
